536bced59aae5098089cc03e54852a48d855f480
Ibrahim Kolawole <kolawoleibrahim100@gmail.com>
2024-11-18 03:05:33 +0100

    Adding of HTML, CSS & Js

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-let result1 = document.querySelector(".result1")
-let result2 = document.querySelector(".result2")
+let isOriginal = true;
 
-function fivePercent(){
-    let billValue =  document.querySelector(".bill-value").value
-    let peopleIcon = document.querySelector(".people-icon").value
-    result1.textContent = "$" + billValue / peopleIcon * 0.05
-    result2.textContent = "$" + billValue / peopleIcon
-}
-function tenPercent(){
-    let billValue =  document.querySelector(".bill-value").value
-    let peopleIcon = document.querySelector(".people-icon").value
-    result1.textContent = "$" + billValue / peopleIcon * 0.1
-    result2.textContent = "$" + billValue / peopleIcon
-}
-function fifteenPercent(){
-    let billValue =  document.querySelector(".bill-value").value
-    let peopleIcon = document.querySelector(".people-icon").value
-    result1.textContent = "$" + billValue / peopleIcon * 0.15
-    result2.textContent = "$" + billValue / peopleIcon
-}
-function twentyfivePercent(){
-    let billValue =  document.querySelector(".bill-value").value2   
-    let peopleIcon = document.querySelector(".people-icon").value
-    result1.textContent = "$" + billValue / peopleIcon * 0.2
-    result2.textContent = "$" + billValue / peopleIcon
-}
-function fiftyPercent(){
-    let billValue =  document.querySelector(".bill-value").value
-    let peopleIcon = document.querySelector(".people-icon").value
-    result1.textContent = "$" + billValue / peopleIcon * 0.5
-    result2.textContent = "$" + billValue / peopleIcon
-}
-function customPercent(){
-    let selectCustom = document.querySelector(".select-custom").value / 100 
-    let billValue =  document.querySelector(".bill-value").value
-    let peopleIcon = document.querySelector(".people-icon").value
-    result1.textContent = "$" + billValue / peopleIcon * selectCustom
-    result2.textContent = "$" + billValue / peopleIcon
+let heading1 = document.getElementById("heading1")
+let heading2 = document.getElementById("heading2")
+let heading3 = document.getElementById("heading3")
+
+function switchContent(){
+    if (isOriginal){
+        heading1.textContent = 19.99
+        heading2.textContent = 24.99
+        heading3.textContent = 39.99
+    } else { 
+       heading1.innerHTML = 199.99
+       heading2.innerHTML = 249.99
+       heading3.innerHTML = 399.99
+    }
+    isOriginal = !isOriginal;
 }
